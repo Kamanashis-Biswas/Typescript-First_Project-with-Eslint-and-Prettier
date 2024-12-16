d345d885ab66dc7695ed4ff09491db08a6275ecb
Kamanshis-Biswas <kamanashis.cse@gmail.com>
2024-12-17 03:57:06 +0600

    Do Raw Search, Raw Filtering, sorting and Limiting

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -5,8 +5,34 @@ import httpStatus from 'http-status';
 import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  console.log('base query', query);
+  const queryObj = { ...query };
+
+  //{email:{$regex:query.searchTerm, $options: 'i'}}
+  //{presentAddress:{$regex:query.searchTerm, $options: 'i'}}
+  //{'name.firstName':{$regex:query.searchTerm, $options: 'i'}}
+
+  const studentSearchableFields = ['email', 'name.firstName', 'presentAddress'];
+
+  let searchTerm = '';
+
+  if (query?.searchTerm) {
+    searchTerm = query?.searchTerm as string;
+  }
+
+  const searchQuery = Student.find({
+    $or: studentSearchableFields.map((field) => {
+      return { [field]: { $regex: searchTerm, $options: 'i' } };
+    }),
+  });
+
+  // filtering
+  const excludedFields = ['searchTerm', 'sort', 'page', 'limit'];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  const filterQuery = searchQuery
+    .find(queryObj)
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
@@ -14,7 +40,22 @@ const getAllStudentsFromDB = async () => {
         path: 'academicFaculty',
       },
     });
-  return result;
+
+  let sort = '-createdAt';
+  if (query?.sort) {
+    sort = query.sort as string;
+  }
+
+  const sortQuery = filterQuery.sort(sort);
+
+  let limit = 1;
+  if (query?.limit) {
+    limit = query.limit;
+  }
+
+  const limitQuery = await sortQuery.limit(limit);
+
+  return limitQuery;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
